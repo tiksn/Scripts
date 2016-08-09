38e3b761e25715b5510d93ede1c09c6ac711d02b
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-08-09 23:34:06 +0300

    standard modules and scripts that i use

---
+++ Install-StandardModulesAndScripts.ps1
@@ -0,0 +1,22 @@
+﻿# Modules
+Install-Module -Name Carbon -Force
+Install-Module -Name CredentialManager -Force
+Install-Module -Name GroceryChecklistToolkit -Force
+Install-Module -Name Microsoft.PowerShell.Archive -Force
+Install-Module -Name Microsoft.WindowsPassportUtilities.Commands -Force
+Install-Module -Name MlkPwgen -Force
+Install-Module -Name NTFSSecurity -Force
+Install-Module -Name Pscx -Force
+Install-Module -Name PSScriptAnalyzer -Force
+Install-Module -Name PSWindowsUpdate -Force
+Install-Module -Name posh-git -Force
+Install-Module -Name PowerShellCookbook -Force
+Install-Module -Name PowerShellHumanizer -Force
+Install-Module -Name Pushalot -Force
+Install-Module -Name ResolveAlias -Force
+Install-Module -Name SeeShell -Force
+Install-Module -Name TIKSN-PowerShell-Cmdlets -Force
+Install-Module -Name WintellectPowerShell -Force
+
+# Scripts
+Install-Script -Name Update-Windows -Force
