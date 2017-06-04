b115298e9adee4a473d7ec2362b1b7116892e100
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-04 04:05:36 +0300

    package added

--- InstallStandardChocolateyPackages.cmd
+++ InstallStandardChocolateyPackages.cmd
@@ -1,6 +1,7 @@
 @echo off
 
-choco install grocery-checklist-shell 
+choco install grocery-checklist-shell
+choco install nuget.commandline
 choco install scriptcs
-choco install sqlite-studio.portable 
+choco install sqlite-studio.portable
 choco install sysinternals
