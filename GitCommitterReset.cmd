3e94121d7de6f8eaa2c31d828aa074fb2cf4ee93
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-06-28 00:22:34 +0300

    new script added

---
+++ GitCommitterReset.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+git config --global --unset user.name
+git config --global --unset user.email
+
+REM git config --local user.name "Tigran TIKSN Torosyan"
+REM git config --local user.email "personal@tiksn.com"
