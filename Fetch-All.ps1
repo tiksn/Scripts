baca050d7a77bd69e50f8e1cbdfacc6b1998c76e
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-10-07 23:14:30 +0300

    updated fetching script

--- Fetch-All.ps1
+++ Fetch-All.ps1
@@ -1,44 +1,55 @@
-﻿cls
-$gitServiceFolders = "Beanstalk", "Bitbucket", "GitBook", "GitHub", "GitLab", "VSTS"
+﻿$gitServiceFolders = @("Bitbucket", "Chudovo", "GitHub", "GitLab", "VSTS")
 
-ForEach($gitServiceFolder in $gitServiceFolders) {
-    #FetchGitRepositories -folder $gitServiceFolder
-    Write-Output $gitServiceFolder
-    ScanDirectory -folder (Get-Item $gitServiceFolder)
+$rootFolder = Join-Path -Path $PSScriptRoot -ChildPath "..\..\.." -Resolve
+
+$gitFolders = New-Object System.Collections.ArrayList
+
+Function ScanDirectory($Folder, $RootScanFolder, $PercentComplete)
+{
+	$FoundGitFolders = New-Object System.Collections.ArrayList
+	
+	Write-Progress -Id 1 -Activity "Detecting git folders" -Status "Scanning service folder $RootScanFolder" -CurrentOperation "Scanning $Folder" -PercentComplete $PercentComplete
+	
+	$gitFolder = Join-Path -Path $Folder -ChildPath '.git'
+	if (Test-Path $gitFolder)
+	{
+		$FoundGitFolders.Add($Folder)
+	}
+	else
+	{
+		$childFolders = Get-ChildItem -Path $Folder -Directory
+		
+		foreach ($childFolder in $childFolders)
+		{
+			$childFoundGitFolders = ScanDirectory -Folder $childFolder.FullName -RootScanFolder $RootScanFolder -PercentComplete $PercentComplete
+			
+			if ($childFoundGitFolders -ne $null)
+			{
+				$FoundGitFolders.AddRange($childFoundGitFolders)
+			}
+		}
+	}
+	
+	return $FoundGitFolders
 }
 
-Function ScanDirectory([System.IO.DirectoryInfo] $folder) {
-    $oldFolder = Get-Location
-    Set-Location $folder
-    Write-Host Get-Location
-    return
-    if(Test-Path '.git') {
-        $cd = Get-Location
-        Set-Location $folder
-        Set-Location $cd.Path
-    } else {
-        #ForEach($subfolder in (Get-ChildItem $folder)) {
-            #ScanDirectory -folder $subfolder
-        #}
-    }
-
-    Set-Location $oldFolder
+function FetchGitRepository($Folder, $PercentComplete)
+{
+	Write-Progress -Id 1 -Activity "Fetching git repository" -Status "Folder $Folder" -PercentComplete $PercentComplete
+	Start-Process -FilePath 'C:\Program Files\Git\bin\git.exe' -ArgumentList 'pull -r -p' -WorkingDirectory $Folder -Wait
 }
 
-function FetchGitRepositories($folder){
-    Set-Location $folder
-    Write-Output Get-Location
-
-    ForEach($subfolder in Get-ChildItem -Path $folder) {
-        FetchGitRepository -folder $subfolder
-    }
-
-    Set-Location ".."
-    Write-Output Get-Location
+$processed = 0
+foreach ($gitServiceFolder in $gitServiceFolders)
+{
+	$processed++
+	$folderToScan = Join-Path -Path $rootFolder -ChildPath $gitServiceFolder
+	$gitFolders += ScanDirectory -Folder (Get-Item $folderToScan) -RootScanFolder $folderToScan -FoundGitFolders $gitFolders -PercentComplete ($processed * 100 / $gitServiceFolders.Length)
 }
 
-function FetchGitRepository($folder){
-}
-
-function FetchDirectory(){
+$processed = 0
+foreach ($gitFolder in $gitFolders)
+{
+	$processed++
+	FetchGitRepository -Folder $gitFolder -PercentComplete ($processed * 100 / $gitFolders.Length)
 }
