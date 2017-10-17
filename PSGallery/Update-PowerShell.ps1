ed62a0125298dc50648b3d393c7532241b4c6bd2
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-10-17 11:40:17 +0300

    added script

---
+++ PSGallery/Update-PowerShell.ps1
@@ -0,0 +1,45 @@
+﻿
+<#PSScriptInfo
+
+.VERSION 1.1.0.0
+
+.GUID aa6f8439-84b2-42b6-81b3-268677c71402
+
+.AUTHOR Tigran TIKSN Thorosyan
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+#Requires -Module PowerShellGet
+
+<# 
+
+.DESCRIPTION 
+ Updates PowerShell scripts, modules and help
+
+#> 
+Param()
+
+Update-Module
+Update-Script
+Update-Help
