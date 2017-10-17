3a2e34658076b629e17e1d1ebb87d9d956e5c9cb
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-10-17 11:03:46 +0300

    Update-Windows updated

---
+++ PSGallery/Update-Windows.ps1
@@ -0,0 +1,60 @@
+﻿
+<#PSScriptInfo
+
+.VERSION 1.1.0.0
+
+.GUID a1f821bb-5100-4e94-89b7-e2d4970e1706
+
+.AUTHOR Tigran TIKSN Thorosyan
+
+.COMPANYNAME 
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+#Requires -Module PSWindowsUpdate
+
+<#
+	.SYNOPSIS
+		A brief description of the Update-Windows.ps1 file.
+	
+	.DESCRIPTION
+		Install Windows Updates
+	
+	.PARAMETER AcceptAll
+		A description of the AcceptAll parameter.
+	
+	.PARAMETER Verbose
+		A description of the Verbose parameter.
+	
+	.PARAMETER Silent
+		A description of the Silent parameter.
+	
+	.NOTES
+		Additional information about the file.
+#>
+param
+(
+	[switch]$AcceptAll,
+	[switch]$Verbose
+)
+
+Get-WUServiceManager | ForEach-Object { Install-WindowsUpdate -ServiceID $_.ServiceID -AcceptAll:$AcceptAll -Verbose:$Verbose }
