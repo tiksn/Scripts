ad4183d7e92f8bdb8e44be934d598e64634eeede
Tigran TIKSN Torosyan <personal@tiksn.com>
2018-01-16 10:22:05 +0200

    variable name fixed

--- GeneratePortNumber.ps1
+++ GeneratePortNumber.ps1
@@ -1,5 +1,5 @@
 ﻿$port = Get-Random -Minimum 49152 -Maximum 65535
 
-Set-Clipboard -Value $key
+Set-Clipboard -Value $port
 
 Write-Output "Generated port number is $port. Port number copied to clipboard"
