b105433ccb3210462946a5bfbb8ce65317531af7
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-12 23:31:13 +0200

    converted to powershell

--- GitCommitterReset.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-git config --global --unset-all user.name
-git config --global --unset-all user.email
-
-REM git config --local user.name "Tigran TIKSN Torosyan"
-REM git config --local user.email "personal@tiksn.com"
---
+++ GitCommitterReset.ps1
@@ -0,0 +1,5 @@
+git config --global --unset-all user.name
+git config --global --unset-all user.email
+
+# git config --local user.name "Tigran TIKSN Torosyan"
+# git config --local user.email "personal@tiksn.com"
