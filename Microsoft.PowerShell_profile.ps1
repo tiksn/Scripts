1c53145a1cc16572ca3d2920c89a91d15ea3d9f4
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-01-26 17:16:27 +0200

    set title

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,17 @@
 function prompt {
     $formattedDate = (Get-Date).ToString("f");
     $formattedDate = "[$(($formattedDate | Out-String).trim())]";
+    $folderName = Split-Path $executionContext.SessionState.Path.CurrentLocation -Leaf
     Try
     {
         $repoStatus = Get-RepositoryStatus;
+        $host.UI.RawUI.WindowTitle = "$($folderName) | $($repoStatus.CurrentBranch) $($repoStatus.Files.Count)";
         return "GIT $($executionContext.SessionState.Path.CurrentLocation) | $($repoStatus.CurrentBranch) $($repoStatus.Files.Count) $('>' * ($nestedPromptLevel + 1)) ";
     }
     Catch
     {
         $repoStatus = $null;
+        $host.UI.RawUI.WindowTitle = "$($folderName)";
         return "PSC $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) ";
     }
 }
