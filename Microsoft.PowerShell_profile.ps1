0a1f538dfe91c61d260c9cb45a842e62e304e26d
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-03-06 12:44:08 +0200

    command starts in new line

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,11 +4,11 @@ function prompt {
     Try
     {
         $repoStatus = Get-RepositoryStatus;
-        return "GIT $($executionContext.SessionState.Path.CurrentLocation) | $($repoStatus.CurrentBranch) $($repoStatus.Files.Count) $('>' * ($nestedPromptLevel + 1)) ";
+        return "GIT $($executionContext.SessionState.Path.CurrentLocation) | $($repoStatus.CurrentBranch) $($repoStatus.Files.Count)`n$('>' * ($nestedPromptLevel + 1)) ";
     }
     Catch
     {
         $repoStatus = $null;
-        return "PSC $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) ";
+        return "PSC $($executionContext.SessionState.Path.CurrentLocation)`n$('>' * ($nestedPromptLevel + 1)) ";
     }
 }
