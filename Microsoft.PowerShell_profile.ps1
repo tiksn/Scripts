2f4e15ed143d90b5d4c5df5cf77e11ad696f0567
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-06-18 22:43:10 +0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,16 +7,25 @@ function prompt {
     Try
     {
         $repoStatus = Get-RepositoryStatus;
+        Write-Host -Object "GIT" -NoNewline -BackgroundColor Yellow -ForegroundColor Red
+        Write-Host -Object " " -NoNewline
+        Write-Host -Object $executionContext.SessionState.Path.CurrentLocation -NoNewline -BackgroundColor Black -ForegroundColor Gray
+        Write-VcsStatus
+        Write-Host
         # return "GIT $($executionContext.SessionState.Path.CurrentLocation) | $($repoStatus.CurrentBranch) $($repoStatus.Files.Count)`n$('>' * ($nestedPromptLevel + 1)) ";
-        return "GIT $($executionContext.SessionState.Path.CurrentLocation) $(Write-VcsStatus)`n$('>' * ($nestedPromptLevel + 1)) ";
     }
     Catch
     {
         $repoStatus = $null;
-        return "PSC $($executionContext.SessionState.Path.CurrentLocation)`n$('>' * ($nestedPromptLevel + 1)) ";
+        Write-Host -Object "PSC" -NoNewline -BackgroundColor Yellow -ForegroundColor Red
+        Write-Host -Object " " -NoNewline
+        Write-Host -Object $executionContext.SessionState.Path.CurrentLocation -NoNewline -BackgroundColor Black -ForegroundColor Gray
+        Write-Host
     }
-}
 
+    Write-Host -Object "$('>' * ($nestedPromptLevel + 1))" -NoNewline
+    return " "
+}
 
 $PowerShellTranscriptsPath = Join-Path -Path $env:userprofile -ChildPath "PowerShellTranscripts"
 
