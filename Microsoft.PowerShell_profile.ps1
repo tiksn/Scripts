0b7c39b3c0ef458b68404f9d536d34532e537fc5
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-06-18 22:03:44 +0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,14 @@
+Import-Module ObjectiveGit
+Import-Module posh-git
+
 function prompt {
     $formattedDate = (Get-Date).ToString("f");
     $formattedDate = "[$(($formattedDate | Out-String).trim())]";
     Try
     {
         $repoStatus = Get-RepositoryStatus;
-        return "GIT $($executionContext.SessionState.Path.CurrentLocation) | $($repoStatus.CurrentBranch) $($repoStatus.Files.Count)`n$('>' * ($nestedPromptLevel + 1)) ";
+        # return "GIT $($executionContext.SessionState.Path.CurrentLocation) | $($repoStatus.CurrentBranch) $($repoStatus.Files.Count)`n$('>' * ($nestedPromptLevel + 1)) ";
+        return "GIT $($executionContext.SessionState.Path.CurrentLocation) $(Write-VcsStatus)`n$('>' * ($nestedPromptLevel + 1)) ";
     }
     Catch
     {
