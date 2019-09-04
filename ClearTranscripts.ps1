bfc950fa9f2df1fdcb9497c2bed725b4c197bdf0
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-04 10:29:41 +0300

    Create ClearTranscripts.ps1

---
+++ ClearTranscripts.ps1
@@ -0,0 +1,16 @@
+$PowerShellTranscriptsPath = Join-Path -Path $HOME -ChildPath "PowerShellTranscripts"
+
+$TranscriptFilePaths = Get-ChildItem $PowerShellTranscriptsPath -File
+
+$TranscriptAgeThreshold = New-TimeSpan -Hours (7 * 24)
+
+foreach ($TranscriptFilePath in $TranscriptFilePaths) {
+    $parts = $TranscriptFilePath.BaseName -split '--'
+    $dateParts = $parts[0] -split '-'
+    $timeParts = $parts[1] -split '-'
+    $TranscriptDate = Get-Date -Year $dateParts[0] -Month $dateParts[1] -Day $dateParts[2] -Hour $timeParts[0] -Minute $timeParts[1] -Second $timeParts[2]
+    $TranscriptAge = New-TimeSpan –Start $TranscriptDate –End (Get-Date)
+    if ($TranscriptAge -gt $TranscriptAgeThreshold) {
+        Remove-ItemSafely $TranscriptFilePath
+    }
+}
