95d3c9b7051cd9a76b824513f2f5354439f0f8e2
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-09-08 13:36:25 +0300

    update system

---
+++ Update-System.ps1
@@ -0,0 +1,24 @@
+Update-Module -Scope AllUsers
+Update-Script
+
+if ($IsWindows) {
+
+}
+
+if ($IsLinux) {
+    $release = Get-Content -Path /etc/os-release
+    $release = $release.Split([Environment]::NewLine) | Where-Object { $_.StartsWith("ID=") }
+    $release = $release.Substring(3)
+
+    $IsFedora = ($release -eq "Fedora")
+    $IsUbuntu = ($release -eq "Ubuntu")
+
+    if ($IsFedora) {
+        dnf check-update
+        dnf update
+
+        flatpak update
+    }
+}
+
+
