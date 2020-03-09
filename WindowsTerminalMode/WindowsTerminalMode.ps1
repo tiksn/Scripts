644c4e1068defe489a7eede3ba3ffb7b834c01c2
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-03-09 20:02:53 +0400

    Update WindowsTerminalMode.ps1

--- WindowsTerminalMode/WindowsTerminalMode.ps1
+++ WindowsTerminalMode/WindowsTerminalMode.ps1
@@ -1,5 +1,27 @@
-$WindowsTerminalMode_Load = {
+$cancelButton_Click = {
+    $WindowsTerminalMode.Close()
+}
 
+Import-Module -Name posh-git
+# Import-Module -Name ObjectiveGit
+
+function GetCurrentBranchValue {
+    param (
+
+    )
+
+    $wtPackage = Get-AppPackage -Name Microsoft.WindowsTerminal
+    $packagesPath = Join-Path -Path $env:LOCALAPPDATA -ChildPath 'Packages'
+    $wtPackageDateFolder = Join-Path -Path $packagesPath -ChildPath $wtPackage.PackageFamilyName
+    $wtGitRepoPath = Join-Path -Path $wtPackageDateFolder -ChildPath "LocalState"
+    $branches = Get-Branch -Repository $wtGitRepoPath
+    $currentBranch = $branches | Where-Object { $_.Current }
+
+    $currentBranchValueLabel.Text = $currentBranch.Name
+}
+
+$WindowsTerminalMode_Load = {
+    GetCurrentBranchValue
 }
 
 . (Join-Path $PSScriptRoot 'WindowsTerminalMode.designer.ps1')
