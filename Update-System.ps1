9b4940e3a53c5bd603dbaa36ee12be1bf84e4834
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-19 12:18:10 +0400

    Update Update-System.ps1

--- Update-System.ps1
+++ Update-System.ps1
@@ -1,18 +1,31 @@
 #Requires -RunAsAdministrator
 
-[CmdletBinding()]
+[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
 Param ()
-
-Update-Module -Scope AllUsers -AcceptLicense -Confirm:$false
-Update-Script -AcceptLicense -Confirm:$false
-Update-Help -Confirm:$false
+Write-Progress -Activity "Updating PowerShell Modules for All Users" -Id 1478576163
+Update-Module -Scope AllUsers -AcceptLicense
+Write-Progress -Activity "Updating PowerShell Modules for Current Users" -Id 1478576163
+Update-Module -Scope CurrentUser -AcceptLicense
+Write-Progress -Activity "Updating PowerShell Scripts" -Id 1478576163
+Update-Script -AcceptLicense
+Write-Progress -Activity "Updating PowerShell Help files" -Id 1478576163
+Update-Help
 
 if ($IsWindows) {
+    Write-Progress -Activity "Updating Defender signatures" -Id 1478576163
     Update-MpSignature
 
-    choco upgrade --confirm all
-    scoop update
+    Write-Progress -Activity "Updating Chocolatey packages" -Id 1478576163
+    if ($PSCmdlet.ShouldProcess("Chocolatey packages", "Update all packages")) {
+        choco upgrade --confirm all
+    }
+
+    Write-Progress -Activity "Updating Scoop packages" -Id 1478576163
+    if ($PSCmdlet.ShouldProcess("Scoop packages", "Update all packages")) {
+        scoop update
+    }
     
+    Write-Progress -Activity "Updating Windows" -Id 1478576163
     Get-WUServiceManager | ForEach-Object { Install-WindowsUpdate -ServiceID $_.ServiceID -AcceptAll }
 }
 
@@ -32,15 +45,19 @@ if ($IsLinux) {
         flatpak update
     }
 
-    if($IsUbuntu -or $IsDebian) {
+    if ($IsUbuntu -or $IsDebian) {
         apt update
         apt upgrade
     }
 }
 
-Write-Verbose 'Updating all .NET Core Global Tools'
+Write-Progress -Activity "Updating all .NET Core Global Tools" -Id 1478576163
 
 foreach ($package in $(dotnet tool list --global | Select-Object -Skip 2)) {
-    dotnet tool update --global $($package.Split(" ", 2)[0])
+    $tool = $package.Split(" ", 2)[0]
+    if ($PSCmdlet.ShouldProcess("DotNet global tool $tool", "Update global tool")) {
+        dotnet tool update --global $tool
+    }
 }
 
+Write-Progress -Activity "The End." -Completed -Id 1478576163
