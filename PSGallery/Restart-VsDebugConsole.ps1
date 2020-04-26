b4830e9c33050d8ca7c1dba0eba0a96a31b2f945
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-26 22:36:26 +0400

    Update Restart-VsDebugConsole.ps1

--- PSGallery/Restart-VsDebugConsole.ps1
+++ PSGallery/Restart-VsDebugConsole.ps1
@@ -1,3 +1,16 @@
+[CmdletBinding()]
+param(
+
+)
+
+function KillProcessTree {
+    Param(
+        [int]$ProcessId
+    )
+    Get-CimInstance Win32_Process | Where-Object { $_.ParentProcessId -eq $ProcessId } | ForEach-Object { KillProcessTree $_.ProcessId }
+    Stop-Process -Id $ProcessId
+}
+
 function CheckProcessAncestor {
     param (
         [Parameter(Mandatory = $true)]
@@ -22,13 +35,15 @@ function CheckProcessAncestor {
 $Processes = Get-Process
 
 $vsDebugConsoleProcesses = $Processes | Where-Object { $_.Name -eq 'VsDebugConsole' }
-
 $vsDebugConsoleProcess = $vsDebugConsoleProcesses | Where-Object { (CheckProcessAncestor -Process $_ -AncestorName 'devenv') -and (-not (CheckProcessAncestor -Process $_ -AncestorName 'WindowsTerminal')) }
+
 if ($null -ne $vsDebugConsoleProcess) {
+    Write-Debug "ID: $($vsDebugConsoleProcess.Id), Name: $($vsDebugConsoleProcess.Name)"
     $winProcesses = Get-CimInstance Win32_Process
     $vsDebugConsoleWinProcess = $winProcesses | Where-Object { $_.ProcessId -eq $vsDebugConsoleProcess.Id }
-    Stop-Process -Id $vsDebugConsoleProcess.Id
-    if ($?) {
-        Start-Process $vsDebugConsoleWinProcess.CommandLine
-    }
+    $vsDebugConsoleProcess | Stop-Process
+    KillProcessTree $vsDebugConsoleProcess.Id
+    # if ($?) {
+    #     Start-Process $vsDebugConsoleWinProcess.CommandLine
+    # }
 }
