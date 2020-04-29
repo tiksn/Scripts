c4fcad90550275d4543e8547c0a4a84c554ad7a9
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-29 11:04:51 +0400

    Create Install-WSLTools.ps1

---
+++ PSGallery/Install-WSLTools.ps1
@@ -0,0 +1,44 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID a954848e-a6f7-4386-b084-bbd9f5862fb8
+
+.AUTHOR Tigran TIKSN Torosyan
+
+.COMPANYNAME
+
+.COPYRIGHT
+
+.TAGS
+
+.LICENSEURI
+
+.PROJECTURI
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+
+
+.PRIVATEDATA
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Inatall WSL Utilities and other tools 
+
+#>
+[CmdletBinding()]
+Param()
+
+
