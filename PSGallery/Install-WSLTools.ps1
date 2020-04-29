62cc990a767e5f1ecdc84cee3e0f3c3c29bf4890
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-29 16:05:01 +0400

    Update Install-WSLTools.ps1

--- PSGallery/Install-WSLTools.ps1
+++ PSGallery/Install-WSLTools.ps1
@@ -44,13 +44,32 @@ Param()
 
 $distributions = wsl --list | Where-Object { $_ -ne $null -and $_ -ne "" } | Select-Object -Skip 1 | ForEach-Object { ($_ -split " ")[0] }
 
+# https://github.com/wslutilities/wslu
 foreach ($distribution in $distributions) {
     switch ($distribution) {
         "Ubuntu" {
-            Write-Host "Ubuntu" 
+            wsl --exec "sudo apt update" --distribution $distribution
+            wsl --exec "sudo apt install ubuntu-wsl" --distribution $distribution
         }
         "Debian" { 
-            Write-Host "Debian" 
+            wsl --exec "sudo apt install gnupg2 apt-transport-https" --distribution $distribution
+            wsl --exec "wget -O - https://access.patrickwu.space/wslu/public.asc | sudo apt-key add -" --distribution $distribution
+            wsl --exec "echo `"deb https://access.patrickwu.space/wslu/debian buster main`" | sudo tee -a /etc/apt/sources.list" --distribution $distribution
+            wsl --exec "sudo apt update" --distribution $distribution
+            wsl --exec "sudo apt install wslu" --distribution $distribution 
+        }
+        Default { Write-Error "$distribution is not supported" }
+    }
+}
+
+# wslfetch
+foreach ($distribution in $distributions) {
+    switch ($distribution) {
+        "Ubuntu" {
+            wsl --exec "wslfetch" --distribution $distribution
+        }
+        "Debian" { 
+            wsl --exec "wslfetch" --distribution $distribution
         }
         Default { Write-Error "$distribution is not supported" }
     }
