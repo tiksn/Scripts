55fbb5006670139d721c2bd73f55ff2fc1626583
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-08-19 14:56:06 +0400

    Update Update-PowerShellCache.ps1

--- Update-PowerShellCache.ps1
+++ Update-PowerShellCache.ps1
@@ -56,9 +56,40 @@ try {
 
     $ProfileCache.Release = Get-PSReleaseCurrent
     $ProfileCache.ReleasePreview = Get-PSReleaseCurrent -Preview
-    
+
+    if ($features.NationalBankOfUkraineRates) {
+        $xml = New-Object xml
+
+        $xml.Load('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange')
+        $exchangeRates = $xml.exchange | Select-Object -ExpandProperty currency
+
+        $yesterdaysDatePattern = (Get-Date).AddDays(-1).ToString("yyyyMMdd")
+
+        $xml.Load("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=$yesterdaysDatePattern")
+        $yesterdaysExchangeRates = $xml.exchange | Select-Object -ExpandProperty currency
+
+        $ProfileCache.NationalBankOfUkraine = @{
+            ExchangeRates           = $exchangeRates
+            YesterdaysExchangeRates = $yesterdaysExchangeRates
+        }
+    }
+
+    if ($features.CentralBankOfArmeniaRates) {
+        $response = Invoke-RestMethod 'https://www.cba.am/_layouts/rssreader.aspx?rss=280F57B8-763C-4EE4-90E0-8136C13E47DA' -Method 'GET' -Headers $headers -Body $body
+        $response = $response | Select-Object -ExpandProperty title
+        $rates = $response | ForEach-Object {
+            $parts = $_ -split '-' | ForEach-Object { $_.Trim() }
+            @{
+                Code = $parts[0]
+                Rate = ($parts[2] -as [decimal]) / ($parts[1] -as [decimal])
+            }
+        }
+
+        $ProfileCache.CentralBankOfArmeniaRates = $rates
+    }
+
     $ProfileCache.Saved = Get-Date
-    $ProfileCache | Format-Custom
+    $ProfileCache | Export-Clixml -Path $PowerShellCachePath
 }
 catch {
     throw $_
