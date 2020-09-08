96e563746f91f6db6d634b121079ef86dd1825fe
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-09-08 15:00:17 +0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -207,6 +207,17 @@ if ($env:WT_SESSION -or $env:TERMINATOR_UUID -or $env:GNOME_TERMINAL_SCREEN) {
         # Get-Command -Name $randomCommand.Name -Syntax
     }
     
+    # PowerShell parameter completion shim for the WinGet
+    Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+        param($wordToComplete, $commandAst, $cursorPosition)
+            [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+            $Local:word = $wordToComplete.Replace('"', '""')
+            $Local:ast = $commandAst.ToString().Replace('"', '""')
+            winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            }
+    }
+
     # PowerShell parameter completion shim for the GitHub CLI
     gh completion --shell powershell | Set-Variable -Name ghCompletion
 
