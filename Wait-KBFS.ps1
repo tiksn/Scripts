9cc6a034e9c788ee073dc4e5663c651eb50ff70b
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-11-17 09:56:05 +0200

    Create Wait-KBFS.ps1

---
+++ Wait-KBFS.ps1
@@ -0,0 +1,20 @@
+[CmdletBinding()]
+param (
+)
+
+while ($true) {
+    try {
+        $drive = keybase kbfsmount get
+        $drivePath = Resolve-Path -Path $drive
+    
+        if (-not $drivePath) {
+            throw 'KBFS drive path could not br resolved'
+        }
+
+        break
+    }
+    catch {
+        Write-Warning -Message "Waiting for KBFS. Please start Keybase manually if it takes too long."
+        Start-Sleep -Seconds 10
+    }
+}
