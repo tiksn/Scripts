a08e984d584becb698eb5db887bce7a20b810c8f
Tigran TIKSN Torosyan <personal@tiksn.com>
2021-03-02 20:16:43 +0200

    add verbose

--- DeduplicateModules.ps1
+++ DeduplicateModules.ps1
@@ -1,3 +1,7 @@
+[CmdletBinding()]
+param (
+)
+
 $allModules = Get-Module -ListAvailable
 $moduleGroups = $allModules | Group-Object -Property Name
 $multiversionModuleGroups = $moduleGroups | Where-Object { $_.Count -gt 1 }
@@ -10,6 +14,7 @@ for ($i = 0; $i -lt $multiversionModuleGroups.Count; $i++) {
     for ($j = 0; $j -lt $olderModules.Count; $j++) {
         $olderModule = $olderModules[$j]
         Write-Progress -Id 734181098 -ParentId 1756233909 -Activity "Uninstalling older version" -Status "$($olderModule.Version) (latest $($latestVersionModule.Version))" -PercentComplete (($j + 1) * 100 / $olderModules.Count)
+        Write-Verbose -Message "Uninstalling older version of $($olderModule.Name) $($olderModule.Version) (latest $($latestVersionModule.Version))"
         Uninstall-Module -Name $olderModule.Name -RequiredVersion $olderModule.Version
     }
 }
