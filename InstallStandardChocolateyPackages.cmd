c4936ceac9e8b413884bcf84c4226beb73eaa682
Tigran TIKSN Torosyan <personal@tiksn.com>
2022-02-04 10:18:51 +0200

    Delete InstallStandardChocolateyPackages.cmd

--- InstallStandardChocolateyPackages.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-choco install cmdermini
-choco install grocery-checklist-shell
-choco install localegraph
-choco install nuget.commandline
-choco install scriptcs
-choco install sqlite-studio.portable
-choco install sysinternals
