9d7058f5a1b351ec88ccdfb16f0235c019e780d7
Tigran TIKSN Torosyan <personal@tiksn.com>
2022-04-26 10:25:20 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -159,7 +159,6 @@ if ($env:WT_PROFILE_ID -eq '{2595cd9c-8f05-55ff-a1d4-93f3041ca67f}') {
     Invoke-Expression (&starship init powershell)
 }
 else {
-    Import-Module oh-my-posh
     # oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/emodipt.omp.json | Invoke-Expression
     # oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/powerlevel10k_lean.omp.json | Invoke-Expression
     # oh-my-posh --init --shell pwsh --config $env:POSH_THEMES_PATH/stelbent.minimal.omp.json | Invoke-Expression
