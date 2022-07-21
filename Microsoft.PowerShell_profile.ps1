6ab5ea2116e54e25c7cc7117dfc46de3f3e6c437
Tigran TIKSN Torosyan <personal@tiksn.com>
2022-07-21 14:24:47 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -148,6 +148,12 @@ function prompt {
     return ' '
 }
 
+# For zoxide v0.8.0+
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
+
 function quit() {
     [CmdletBinding()]
     param (
