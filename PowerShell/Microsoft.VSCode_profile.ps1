6803dcd71431aa52b76098bd5bbbdbdb12f37683
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-01-30 13:39:59 +0100

    Update Microsoft.VSCode_profile.ps1

--- PowerShell/Microsoft.VSCode_profile.ps1
+++ PowerShell/Microsoft.VSCode_profile.ps1
@@ -1,1 +1,9 @@
 Invoke-Expression (&starship init powershell)
+
+Register-EditorCommand -Name IB1 -DisplayName 'Invoke task' -ScriptBlock {
+    Invoke-TaskFromVSCode.ps1
+}
+
+Register-EditorCommand -Name IB2 -DisplayName 'Invoke task in console' -SuppressOutput -ScriptBlock {
+    Invoke-TaskFromVSCode.ps1 -Console
+}
