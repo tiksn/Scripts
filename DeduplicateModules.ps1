a1c30ab22bb2aaf176d90ee507578dfbbbaa3f31
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-03-09 12:08:41 +0100

    Update DeduplicateModules.ps1

--- DeduplicateModules.ps1
+++ DeduplicateModules.ps1
@@ -8,13 +8,17 @@ $multiversionModuleGroups = $moduleGroups | Where-Object { $_.Count -gt 1 }
 
 for ($i = 0; $i -lt $multiversionModuleGroups.Count; $i++) {
     $multiversionModuleGroup = $multiversionModuleGroups[$i]
-    Write-Progress -Id 1756233909 -Activity 'Uninstalling older versions' -Status $multiversionModuleGroup.Name -PercentComplete (($i + 1) * 100 / $multiversionModuleGroups.Count)
+    $multiversionPID = Get-Random
+    Write-Progress -Id $multiversionPID -Activity 'Uninstalling older versions' -Status $multiversionModuleGroup.Name -PercentComplete (($i + 1) * 100 / $multiversionModuleGroups.Count)
     $latestVersionModule = $multiversionModuleGroup.Group | Sort-Object -Descending -Property Version | Select-Object -First 1
     $olderModules = $multiversionModuleGroup.Group | Where-Object { $_.Version -lt $latestVersionModule.Version } | Sort-Object -Property Version
     for ($j = 0; $j -lt $olderModules.Count; $j++) {
         $olderModule = $olderModules[$j]
-        Write-Progress -Id 734181098 -ParentId 1756233909 -Activity "Uninstalling older version" -Status "$($olderModule.Version) (latest $($latestVersionModule.Version))" -PercentComplete (($j + 1) * 100 / $olderModules.Count)
         Write-Verbose -Message "Uninstalling older version of $($olderModule.Name) $($olderModule.Version) (latest $($latestVersionModule.Version))"
+        $versionPID = Get-Random
+        Write-Progress -Id $versionPID -ParentId $multiversionPID -Activity 'Uninstalling older version' -Status "$($olderModule.Version) (latest $($latestVersionModule.Version))" -PercentComplete (($j + 1) * 100 / $olderModules.Count)
         Uninstall-Module -Name $olderModule.Name -RequiredVersion $olderModule.Version
+        Write-Progress -Id $versionPID -Activity 'Uninstalling older version' -Completed
     }
+    Write-Progress -Id $multiversionPID -Activity 'Uninstalling older versions' -Completed
 }
