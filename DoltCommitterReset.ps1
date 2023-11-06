909865d6215c766e812cd86fe519dc7ffe77cef0
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-06 16:28:43 +0100

    Create DoltCommitterReset.ps1

---
+++ DoltCommitterReset.ps1
@@ -0,0 +1,5 @@
+dolt config --global --unset user.name
+dolt config --global --unset user.email
+
+# dolt config --local --add user.name "Tigran TIKSN Torosyan"
+# dolt config --local --add user.email "personal@tiksn.com"
