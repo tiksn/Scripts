1fc2eb2363063a55f2ff60c4fe4dca50730fe99d
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-03-30 15:48:31 +0100

    Update ClearPersonalNuGetCache.ps1

--- ClearPersonalNuGetCache.ps1
+++ ClearPersonalNuGetCache.ps1
@@ -2,31 +2,33 @@ $UserFolder = [Environment]::GetFolderPath('UserProfile')
 
 $NuGetPackagesFolder = Join-Path -Path $UserFolder -ChildPath '.nuget\packages'
 
-$NuGetPackagesFolders = Get-ChildItem -Path $NuGetPackagesFolder
-$NuSpecPaths = foreach ($NuGetPackageFolder in $NuGetPackagesFolders) {
-    $NuGetPackageVersionFolders = Get-ChildItem -Path $NuGetPackageFolder
-    foreach ($NuGetPackageVersionFolder in $NuGetPackageVersionFolders) {
-        $nuspecFiles = Get-ChildItem -Path $NuGetPackageVersionFolder -Filter *.nuspec
-        $nuspecFiles
+if (Test-Path -Path $NuGetPackagesFolder) {
+    $NuGetPackagesFolders = Get-ChildItem -Path $NuGetPackagesFolder
+    $NuSpecPaths = foreach ($NuGetPackageFolder in $NuGetPackagesFolders) {
+        $NuGetPackageVersionFolders = Get-ChildItem -Path $NuGetPackageFolder
+        foreach ($NuGetPackageVersionFolder in $NuGetPackageVersionFolders) {
+            $nuspecFiles = Get-ChildItem -Path $NuGetPackageVersionFolder -Filter *.nuspec
+            $nuspecFiles
+        }
     }
-}
 
-$MyPackageIds = $NuSpecPaths | ForEach-Object {
-    [xml]$xml = Get-Content -Path $PSItem
-    $packageElement = $xml.DocumentElement | Where-Object { $PSItem.Name -eq 'package' }
-    $metadataElement = $packageElement.ChildNodes | Where-Object { $PSItem.Name -eq 'metadata' }
-    $idElement = $metadataElement.ChildNodes | Where-Object { $PSItem.Name -eq 'id' }
-    $authorsElement = $metadataElement.ChildNodes | Where-Object { $PSItem.Name -eq 'authors' }
-    if (('TIKSN' -eq $authorsElement.InnerText) -or ('Tigran TIKSN Torosyan' -eq $authorsElement.InnerText) -or ($authorsElement.InnerText -like 'TIKSN.*')) {
-        $idElement.InnerText
+    $MyPackageIds = $NuSpecPaths | ForEach-Object {
+        [xml]$xml = Get-Content -Path $PSItem
+        $packageElement = $xml.DocumentElement | Where-Object { $PSItem.Name -eq 'package' }
+        $metadataElement = $packageElement.ChildNodes | Where-Object { $PSItem.Name -eq 'metadata' }
+        $idElement = $metadataElement.ChildNodes | Where-Object { $PSItem.Name -eq 'id' }
+        $authorsElement = $metadataElement.ChildNodes | Where-Object { $PSItem.Name -eq 'authors' }
+        if (('TIKSN' -eq $authorsElement.InnerText) -or ('Tigran TIKSN Torosyan' -eq $authorsElement.InnerText) -or ($authorsElement.InnerText -like 'TIKSN.*')) {
+            $idElement.InnerText
+        }
     }
-}
-| Select-Object -Unique
+    | Select-Object -Unique
 
-foreach ($MyPackageId in $MyPackageIds) {
-    $PackagePath = Join-Path -Path $NuGetPackagesFolder -ChildPath $MyPackageId
-    if (Test-Path -Path $PackagePath) {
-        Write-Host "Deleting $PackagePath"
-        Remove-Item -Path $PackagePath -Force -Recurse
+    foreach ($MyPackageId in $MyPackageIds) {
+        $PackagePath = Join-Path -Path $NuGetPackagesFolder -ChildPath $MyPackageId
+        if (Test-Path -Path $PackagePath) {
+            Write-Host "Deleting $PackagePath"
+            Remove-Item -Path $PackagePath -Force -Recurse
+        }
     }
 }
