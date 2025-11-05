c2194be0361d5de8f0c9a24f4daedfe8b17a9278
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-11-05 12:24:41 -0600

    Update New-SnowflakeLite.ps1

--- PSGallery/New-SnowflakeLite.ps1
+++ PSGallery/New-SnowflakeLite.ps1
@@ -1,7 +1,7 @@
 
 <#PSScriptInfo
 
-.VERSION 1.0.0
+.VERSION 1.1.0
 
 .GUID 60c7add9-8abb-413c-aadc-9f3643e1e56e
 
@@ -40,24 +40,30 @@
  Creates Integer Snowflake based on seconds
 
 #>
-Param()
+[CmdletBinding()]
+param(
+    [switch]$CopyToClipboard
+)
 
 
 $epochStarts = Get-Date -Year 2024 -Month 1 -Day 1 -Hour 0 -Minute 0 -Second 0 -Millisecond 0
 $epochStartsString = $epochStarts | Get-Date -Format 'u'
-Write-PSFMessage -Level Important -Message "Epoch starts $epochStartsString"
+Write-PSFMessage -Level Verbose -Message "Epoch starts $epochStartsString"
 
 $epochEnds = $epochStarts + [System.TimeSpan]::FromSeconds([System.Int32]::MaxValue)
 $epochEndsString = $epochEnds | Get-Date -Format 'u'
-Write-PSFMessage -Level Important -Message "Epoch ends $epochEndsString"
-
+Write-PSFMessage -Level Verbose -Message "Epoch ends $epochEndsString"
 $instantNow = Get-Date
 $instantNowString = $instantNow | Get-Date -Format 'u'
-Write-PSFMessage -Level Important -Message "Now is $instantNowString"
+Write-PSFMessage -Level Verbose -Message "Now is $instantNowString"
 
 $passed = $instantNow - $epochStarts
 $passedTotalSeconds = [int]$passed.TotalSeconds
 
-Write-PSFMessage -Level Important -Message "Total $passedTotalSeconds Seconds passed"
-$passedTotalSeconds | Set-Clipboard
-Write-PSFMessage -Level Important -Message "Copied $passedTotalSeconds to Clipboard"
+Write-PSFMessage -Level Verbose -Message "Total $passedTotalSeconds Seconds passed"
+if ( $CopyToClipboard) {
+    $passedTotalSeconds | Set-Clipboard
+    Write-PSFMessage -Level Important -Message "Copied $passedTotalSeconds to Clipboard"
+}
+
+Write-Output $passedTotalSeconds
